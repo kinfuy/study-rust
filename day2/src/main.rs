e884f9b2dc6300b8058a04d1a8222305097503ae
kinfuy <yonghu520@outlook.com>
2023-04-02 17:52:27 +0800

    fix: detail

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,13 +1,61 @@
 fn main() {
-    fn add(a: &u32, b: u32) -> u32 {
-        a + b
+    // let a = 1;
+    // let b: u32 = 2;
+    // let c = true;
+    // let d = 'c';
+    // let e = "cc";
+
+    // const TEST: &str = "TEST_CONST";
+
+    // let arr = [3; 5];
+    // // let arr = [3; 10];
+
+    // fn test(str: &str) {
+    //     println!("test:{}", &str);
+    // }
+    // test(TEST);
+
+    // println!("{}", TEST);
+
+    // println!("{arr:?}");
+
+    // println!("{}", arr[1]);
+
+    // let tup = (1, 2, 3);
+
+    // println!("{}", tup.0);
+
+    // println!("{arr:?}");
+
+    // println!("{tup:?}");
+    // print!("{},{},{},{},{}", a, b, c, d, e);
+
+    // let condition = true;
+    // let number = if condition { 5 } else { 6 };
+
+    // println!("The value of number is: {number}");
+
+    // let mut count = 0;
+    // 'test: loop {
+    //     println!("count = {count}");
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("remaining = {remaining}");
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'test;
+    //         }
+    //         remaining -= 1;
+    //     }
+
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+
+    for i in 1..4 {
+        println!("{}", i);
     }
-    let mut rst = 0;
-    loop {
-        rst = add(&rst, 3);
-        if rst > 100 {
-            break;
-        }
-    }
-    print!("rst:{}", rst)
 }
