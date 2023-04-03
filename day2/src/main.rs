e8a1eb30d9d74e1c8d8c0cf4762db5e430f3724b
kinfuy <yonghu520@outlook.com>
2023-04-03 21:42:04 +0800

    fix: detail

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,61 +1,88 @@
-fn main() {
-    // let a = 1;
-    // let b: u32 = 2;
-    // let c = true;
-    // let d = 'c';
-    // let e = "cc";
+// fn main() {
+//     // let a = 1;
+//     // let b: u32 = 2;
+//     // let c = true;
+//     // let d = 'c';
+//     // let e = "cc";
 
-    // const TEST: &str = "TEST_CONST";
+//     // const TEST: &str = "TEST_CONST";
 
-    // let arr = [3; 5];
-    // // let arr = [3; 10];
+//     // let arr = [3; 5];
+//     // // let arr = [3; 10];
 
-    // fn test(str: &str) {
-    //     println!("test:{}", &str);
-    // }
-    // test(TEST);
+//     // fn test(str: &str) {
+//     //     println!("test:{}", &str);
+//     // }
+//     // test(TEST);
 
-    // println!("{}", TEST);
+//     // println!("{}", TEST);
 
-    // println!("{arr:?}");
+//     // println!("{arr:?}");
 
-    // println!("{}", arr[1]);
+//     // println!("{}", arr[1]);
 
-    // let tup = (1, 2, 3);
+//     // let tup = (1, 2, 3);
 
-    // println!("{}", tup.0);
+//     // println!("{}", tup.0);
 
-    // println!("{arr:?}");
+//     // println!("{arr:?}");
 
-    // println!("{tup:?}");
-    // print!("{},{},{},{},{}", a, b, c, d, e);
+//     // println!("{tup:?}");
+//     // print!("{},{},{},{},{}", a, b, c, d, e);
 
-    // let condition = true;
-    // let number = if condition { 5 } else { 6 };
+//     // let condition = true;
+//     // let number = if condition { 5 } else { 6 };
 
-    // println!("The value of number is: {number}");
+//     // println!("The value of number is: {number}");
 
-    // let mut count = 0;
-    // 'test: loop {
-    //     println!("count = {count}");
-    //     let mut remaining = 10;
+//     // let mut count = 0;
+//     // 'test: loop {
+//     //     println!("count = {count}");
+//     //     let mut remaining = 10;
 
-    //     loop {
-    //         println!("remaining = {remaining}");
-    //         if remaining == 9 {
-    //             break;
-    //         }
-    //         if count == 2 {
-    //             break 'test;
-    //         }
-    //         remaining -= 1;
-    //     }
+//     //     loop {
+//     //         println!("remaining = {remaining}");
+//     //         if remaining == 9 {
+//     //             break;
+//     //         }
+//     //         if count == 2 {
+//     //             break 'test;
+//     //         }
+//     //         remaining -= 1;
+//     //     }
 
-    //     count += 1;
-    // }
-    // println!("End count = {count}");
+//     //     count += 1;
+//     // }
+//     // println!("End count = {count}");
 
-    for i in 1..4 {
-        println!("{}", i);
+//     let s1 = String::from("hello");
+//     let s2 = s1.clone();
+
+//     println!("s1 = {}, s2 = {}", s1, s2);
+
+//     // let mut s = String::from("Hello");
+//     // s.push_str(", World!");
+//     // println!("{}", s);
+// }
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
     }
+
+    &s[..]
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
+
+    s.clear(); // 错误！
 }
